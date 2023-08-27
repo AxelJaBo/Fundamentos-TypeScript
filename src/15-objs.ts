46c3275944438445682ae8675b1f012721a65434
Daniel <axel.910921@gmail.com>
2023-08-27 07:37:32 -0700

    Objetos como tipos

---
+++ src/15-objs.ts
@@ -0,0 +1,22 @@
+(() => {
+  type Sizes = 'S' | 'M' | 'L' | 'XL';
+  type Product = {
+    title: string,
+    createAt: Date,
+    stock: number,
+    size?: Sizes
+  };
+
+  const products: Product[] = [];
+
+  const addProduct = (data: Product) => {
+    products.push(data);
+  }
+
+  addProduct({
+    title: 'Pro1',
+    createAt: new Date(1991, 1, 1),
+    stock: 12
+  });
+  console.log(products);
+})();
